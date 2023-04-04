c0d95e51ab958a4c4cef942892085b641efe560f
Nipuna Perera <nipuna.royal@gmail.com>
2023-04-04 03:40:38 +0000

    additional lazyvim configs

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true, desc = "Better Escape" })
+vim.keymap.set("t", "jk", "[[<C-><C-n>]]", { noremap = true, silent = true, desc = "Better Escape" })
+vim.keymap.set({ "v", "n", "s" }, "<leader>fs", "<cmd>w<cr><esc>", { desc = "Save File" })
---
+++ dot_config/nvim/lua/plugins/masonlsp.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "gopls",
+        "delve",
+        "terraform-ls",
+        "tflint",
+        "yamlfmt",
+        "yamllint",
+        "typescript-language-server",
+        "jdtls",
+        "golangci-lint-langserver",
+        "go-debug-adapter",
+      },
+    },
+  },
+}
