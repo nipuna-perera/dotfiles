cd8b97396beb637f3101a0d03c9d123f419ab1c6
Nipuna perera <nipuna.royal@gmail.com>
2023-04-05 05:15:30 +0000

    update treesitter and aqua

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,15 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
-    vim.list_extend(opts.ensure_installed, {
-      "go",
-      "toml",
-      "java",
-      "dockerfile",
-    })
+    opts.ignore_install = { "help" }
+
+    if type(opts.ensure_installed) == "table" then
+      vim.list_extend(opts.ensure_installed, {
+        "go",
+        "toml",
+        "java",
+        "dockerfile",
+      })
+    end
   end,
 }
