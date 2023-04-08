d1f4060f9f5019a9a14a6f3b85f216db4cd4d49f
Nipuna perera <nipuna.royal@gmail.com>
2023-04-08 05:43:56 +0000

    Adding native fzf

---
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,20 @@
+-- change some telescope options and a keymap to browse plugin files
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+      -- add a keymap to browse plugin files
+      -- stylua: ignore
+      {
+        "<leader>fp",
+        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+        desc = "Find Plugin File",
+      },
+  },
+  dependencies = {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    config = function()
+      require("telescope").load_extension("fzf")
+    end,
+  },
+}
