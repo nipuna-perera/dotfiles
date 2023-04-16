8a6b549af24fa20a054aa02602c51db5093cb082
Nipuna perera <nipuna.royal@gmail.com>
2023-04-16 16:25:37 +0000

    update to lazygit 2.0

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
-    opts.ignore_install = { "help" }
-
     if type(opts.ensure_installed) == "table" then
       vim.list_extend(opts.ensure_installed, {
         "go",
