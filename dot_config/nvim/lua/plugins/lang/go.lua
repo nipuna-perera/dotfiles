d8d935f8603121127b47c28d5a7e98c9741b9829
Nipuna perera <nipuna.royal@gmail.com>
2023-04-22 14:06:01 -0500

    gopls updates

--- dot_config/nvim/lua/plugins/lang/go.lua
+++ dot_config/nvim/lua/plugins/lang/go.lua
@@ -10,6 +10,52 @@ return {
     end,
   },
   {
+    "neovim/nvim-lspconfig",
+    opts = {
+      setup = {
+        gopls = function(_, opts)
+          local ih = require("inlay-hints")
+          require("lazyvim.util").on_attach(function(client, bufnr)
+            if client.name == "gopls" then
+              ih.on_attach(client, bufnr)
+              -- workaround for gopls not supporting semanticTokensProvider
+              -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+              if not client.server_capabilities.semanticTokensProvider then
+                local semantic = client.config.capabilities.textDocument.semanticTokens
+                client.server_capabilities.semanticTokensProvider = {
+                  full = true,
+                  legend = {
+                    tokenTypes = semantic.tokenTypes,
+                    tokenModifiers = semantic.tokenModifiers,
+                  },
+                  range = true,
+                }
+              end
+            end
+          end)
+          opts.settings = {
+            gopls = {
+              semanticTokens = true,
+              analyses = {
+                unusedparams = true,
+              },
+              staticcheck = true,
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+            },
+          }
+        end,
+      },
+    },
+  },
+  {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
