1e30f242aab614b5250e7af358228ed6c3b7c942
Nipuna Perera <nipuna.royal@gmail.com>
2023-04-25 12:27:01 -0500

    update theming

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,4 +1,25 @@
 return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "gopls",
+        "terraform-ls",
+        "tflint",
+        "yamlfmt",
+        "yamllint",
+        "typescript-language-server",
+        "jdtls",
+        "golangci-lint-langserver",
+        "go-debug-adapter",
+        "ansible-language-server",
+        "ansible-lint",
+      },
+    },
+  },
   -- inlay hints
   {
     "simrat39/inlay-hints.nvim",
--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -21,6 +21,14 @@ return {
     opts = {
       defaults = {
         layout_strategy = "horizontal",
+        sorting_strategy = "ascending",
+        layout_config = {
+          horizontal = { prompt_position = "top", preview_width = 0.55, results_width = 0.8 },
+          vertical = { mirror = false },
+          width = 0.87,
+          height = 0.80,
+          preview_cutoff = 120,
+        },
         winblend = 0,
       },
     },
