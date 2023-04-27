2fb2fb3fb03e7cbbdc28598d26c8c4b8ce8ea01c
Nipuna perera <nipuna.royal@gmail.com>
2023-04-26 23:43:53 -0500

    add tmux stuff

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -6,6 +6,15 @@ return {
     },
   },
   {
+    "christoomey/vim-tmux-navigator",
+    keys = {
+      { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", mode = "n", "window left" },
+      { "<C-l>", "<cmd>TmuxNavigateRight<cr>", mode = "n", "window right" },
+      { "<C-j>", "<cmd>TmuxNavigateDown<cr>", mode = "n", "window down" },
+      { "<C-k>", "<cmd>TmuxNavigateUp<cr>", mode = "n", "window up" },
+    },
+  },
+  {
     "simrat39/symbols-outline.nvim",
     cmd = "SymbolsOutline",
     keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
