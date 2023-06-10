b82dcdb257d7f624751efaff4629e4afcd550646
Nipuna Perera <nipuna.royal@gmail.com>
2023-06-10 08:52:40 -0500

    update nvim and tmux

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -10,16 +10,47 @@ return {
         function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
         desc = "Find Plugin File",
       },
+      {
+        "<leader>fh",
+        function()
+          require("telescope.builtin").find_files({
+            hidden = true,
+            no_ignore = true,
+            no_ignore_parent = true,
+            find_command = { "rg", "--files", "-g", "!{.git,node_modules,.gradle,tmp,dist,test-results}" },
+          })
+        end,
+        desc = "Find Files (hidden included)",
+      },
     },
+    -- dependencies = {
+    --   "nvim-telescope/telescope-fzf-native.nvim",
+    --   build = "make",
+    --   config = function()
+    --     require("telescope").load_extension("fzf")
+    --   end,
+    -- },
+
+    -- fuzzy finder prioritizing filenames and smartcase search
     dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
+      "natecraddock/telescope-zf-native.nvim",
       config = function()
-        require("telescope").load_extension("fzf")
+        require("telescope").load_extension("zf-native")
       end,
     },
     opts = {
       defaults = {
+        -- show me the filename first
+        path_display = function(_, rawPath)
+          local utils = require("telescope.utils")
+          local tail = utils.path_tail(rawPath)
+          local path = utils.transform_path({
+            path_display = {
+              absolute = 0,
+            },
+          }, rawPath)
+          return string.format("%s | %s", tail, path)
+        end,
         layout_strategy = "horizontal",
         sorting_strategy = "ascending",
         layout_config = {
@@ -30,6 +61,11 @@ return {
           preview_cutoff = 120,
         },
         winblend = 0,
+      },
+      pickers = {
+        buffers = {
+          sort_mru = true,
+        },
       },
     },
   },
--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,15 +1,18 @@
 return {
-  {
-    "projekt0n/github-nvim-theme",
-    priority = 1000,
-    config = function()
-      require("github-theme").setup({})
-    end,
-  },
+  -- {
+  --   "projekt0n/github-nvim-theme",
+  --   priority = 1000,
+  --   config = function()
+  --     require("github-theme").setup({})
+  --   end,
+  -- },
+  -- {
+  --   "catppuccin/nvim",
+  -- },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "github_dark_high_contrast",
+      colorscheme = "tokyonight-night",
     },
   },
   {
@@ -19,6 +22,83 @@ return {
       { "<C-l>", "<cmd>TmuxNavigateRight<cr>", mode = "n", "window right" },
       { "<C-j>", "<cmd>TmuxNavigateDown<cr>", mode = "n", "window down" },
       { "<C-k>", "<cmd>TmuxNavigateUp<cr>", mode = "n", "window up" },
+    },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      default_component_configs = {
+        name = {
+          use_git_status_colors = false,
+        },
+        icon = {
+          folder_empty = "󰜌",
+          folder_empty_open = "󰜌",
+        },
+        document_symbols = {
+          kinds = {
+            File = { icon = "󰈙", hl = "Tag" },
+            Namespace = { icon = "󰌗", hl = "Include" },
+            Package = { icon = "󰏖", hl = "Label" },
+            Class = { icon = "󰌗", hl = "Include" },
+            Property = { icon = "󰆧", hl = "@property" },
+            Enum = { icon = "󰒻", hl = "@number" },
+            Function = { icon = "󰊕", hl = "Function" },
+            String = { icon = "󰀬", hl = "String" },
+            Number = { icon = "󰎠", hl = "Number" },
+            Array = { icon = "󰅪", hl = "Type" },
+            Object = { icon = "󰅩", hl = "Type" },
+            Key = { icon = "󰌋", hl = "" },
+            Struct = { icon = "󰌗", hl = "Type" },
+            Operator = { icon = "󰆕", hl = "Operator" },
+            TypeParameter = { icon = "󰊄", hl = "Type" },
+            StaticMethod = { icon = "󰠄 ", hl = "Function" },
+          },
+        },
+        git_status = {
+          symbols = {
+            renamed = "R",
+            untracked = "U",
+            modified = "M",
+            deleted = "D",
+            conflict = "C",
+          },
+        },
+      },
+      renderers = {
+        file = {
+          { "icon" },
+          {
+            "container",
+            width = "100%",
+            right_padding = 1,
+            content = {
+              {
+                "name",
+                use_git_status_colors = true,
+                zindex = 10,
+              },
+              { "diagnostics", zindex = 20, align = "right" },
+              { "git_status", zindex = 20, align = "right" },
+            },
+          },
+        },
+        directory = {
+          { "icon" },
+          {
+            "container",
+            width = "100%",
+            right_padding = 1,
+            content = {
+              {
+                "name",
+                use_git_status_colors = true,
+                zindex = 10,
+              },
+            },
+          },
+        },
+      },
     },
   },
   {
